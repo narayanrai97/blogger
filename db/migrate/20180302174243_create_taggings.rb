97fc2a569ea7073b69f14abd68302c08c22c477a
Narayan Rai <narayanrai97@gmail.com>
2018-03-15 15:52:15 +0000

    Another prepare to Heroku deploy

--- db/migrate/20180302174243_create_taggings.rb
+++ db/migrate/20180302174243_create_taggings.rb
@@ -2,7 +2,7 @@ class CreateTaggings < ActiveRecord::Migration[5.1]
   def change
     create_table :taggings do |t|
       t.references :tag_id, foreign_key: true
-      t.references :article_id, foreign_key: true
+      t.references :articles, foreign_key: true
 
       t.timestamps
     end
