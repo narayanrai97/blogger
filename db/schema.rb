0a12990c7fca7645e532fdb8cbb78f9e59cac851
Narayan Rai <narayanrai97@gmail.com>
2018-07-16 20:46:37 +0000

    make ready for api creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180430182830) do
+ActiveRecord::Schema.define(version: 20180501030730) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20180430182830) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
-    t.text "body"
+    t.text "content"
     t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
