a87d6d9750f3a7cab16b69bcdc7f7eff1a4ae74b
Narayan Rai <narayanrai97@gmail.com>
2018-07-31 19:20:09 +0000

    configure databases for all environments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     # Returns the full title on a per-page basis.
-    def full_title(page_title = '')
+    def full_title(page_title)
         base_title = "Blogger Me App"
         if page_title.empty?
             base_title
