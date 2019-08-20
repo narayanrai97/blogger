6ac5c32aaadd765ba36d135a6ad4f4f93eefdfce
Narayan Rai <narayanrai97@gmail.com>
2019-08-20 14:53:05 -0400

    Create Message controller with actions

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,51 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+
+  def index
+    @messages = Message.paginate(page: params[:page], per_page: 5).order(created_at: :desc)
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:notice] = "Message created."
+      redirect_to messages_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @message.update_attributes(message_params)
+      flash[:notice] = "Message updated."
+      redirect_to messages_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    flash[:success] = "Message deleted."
+    redirect_to messages_path
+  end
+
+
+
+  private
+
+    def message_params
+      params.require(:message).permit(:title, :body, :posted, :message_type, :titulo, :cuerpo)
+    end
+
+    def set_message
+      @message = Message.find(params[:id])
+    end
+end
