77730d083e88f6aeee9fc7b4001e909a39a73d51
Narayan Rai <narayanrai97@gmail.com>
2019-08-22 18:16:58 -0400

    Add some app stylings throughout including buttons

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,13 +1,15 @@
 class PhotosController < ApplicationController
+  before_action :admin_user,     except: [:index, :show]
+
   def index
     @photos = Photo.paginate(page: params[:page])
   end
-  
+
   def new
     @photo = Photo.new
   end
-  
-  def create 
+
+  def create
     @photo = Photo.new(photo_params)
     if @photo.save
       flash[:notice] = "Photo created"
@@ -16,15 +18,15 @@ class PhotosController < ApplicationController
       render 'new'
     end
   end
-  
+
   def show
     @photo = Photo.find(params[:id])
   end
-  
+
   def edit
     @photo = Photo.find(params[:id])
   end
-  
+
   def update
     @photo = Photo.find(params[:id])
     @photo.update(photo_params)
@@ -35,7 +37,7 @@ class PhotosController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @photo = Photo.find(params[:id])
     if @photo.destroy
@@ -45,13 +47,18 @@ class PhotosController < ApplicationController
       render @photo
     end
   end
-  
-  
+
+
   private
-  
+
   def photo_params
     params.require(:photo)
           .permit(:title, :image)
   end
-  
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
 end
