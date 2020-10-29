5241e1bbf9637b7db7c9b26d39d0717ab351f4f6
Narayan Rai <narayanrai97@gmail.com>
2020-10-28 21:26:45 -0400

    Load author from article instance variable

--- app/mailers/author_mailer.rb
+++ app/mailers/author_mailer.rb
@@ -4,7 +4,7 @@ class AuthorMailer < ApplicationMailer
   def article_comment(article)
     # @author = params[:author]
     @article = article
-    @author = article.author
+    @author = @article.author
     @url = 'http://localhost:3000'
     mail(to: @author.email, subject: 'Your article commented!')
   end
